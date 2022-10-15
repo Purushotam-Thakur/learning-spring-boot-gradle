18542402ffd376b8621e09ae75ffab4e1ed43d95
Purushotam Thakur <purushotam261850@gmail.com>
2022-10-15 09:30:15 +0530

    initialCommit

---
+++ src/main/java/com/itsme/learningspringbootgradle/LearningSpringBootGradleApplication.java
@@ -0,0 +1,13 @@
+package com.itsme.learningspringbootgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringBootGradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningSpringBootGradleApplication.class, args);
+    }
+
+}
